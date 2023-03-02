22e3ab30b8208d1b2a32d20fe462e8992373ccd3
lilianbrinzan <126773002+lilianbrinzan@users.noreply.github.com>
2023-03-02 15:20:24 +0100

    lesson update

--- Lektion32/code/coffee_mashine/src/Main.java
+++ Lektion32/code/coffee_mashine/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Добрый день!");
         System.out.println("Добро пожаловать в нашу кофейню!");
         Scanner sc = new Scanner(System.in);
         enum CoffeeType {
